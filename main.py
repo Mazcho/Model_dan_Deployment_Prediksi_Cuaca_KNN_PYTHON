40e96558017a363fe0dcceab4d7a81331bbce9be
Mazcho <putrantonicholaus@gmail.com>
2023-09-19 12:46:55 +0700

    update app

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import numpy as np
 import pickle
 from sklearn.neighbors import KNeighborsClassifier
 
-#insert header gambar
-st.image('Assets\weather-header-NEW-2.jpg')
-
 #open file css
 with open('style.css')as f:
     st.markdown(f'<style>{f.read()}<style>',unsafe_allow_html = True)
+
+#call model
+model = pickle.load(open("model.pkl","rb"))
 
 
 # Using "with" notation
@@ -19,11 +19,17 @@ with st.sidebar:
 if menuapp == "Menu Utama":
     #membuat container
     ct1 = st.container()
-    with ct1:
+    with ct1:   
+        #insert header gambar
+        st.image('Assets\weather-header-NEW-2.jpg')
         st.header('Perubahan Cuaca Selama 3 tahun')
         st.write("By : Nicholaus verdhy Putranto || A11.2020.12447")
     st.divider()
-    st.markdown(""" Dalam kurun waktu 2012 hingga 2015, perubahan cuaca yang ada
+    st.markdown(""" Cuaca di Australia antara tahun 2012 hingga 2015 telah menjadi topik yang sangat penting dan perhatian. Selama periode ini, Australia mengalami berbagai variasi cuaca yang mencolok. Di beberapa wilayah, kekeringan yang parah menjadi masalah serius, dengan pasokan air yang menipis dan tanah yang retak akibat kurangnya hujan. Hal ini memicu kebakaran hutan yang merusak serta mengganggu kehidupan manusia dan satwa liar.
+
+Namun, ada juga sisi lain dari koin cuaca Australia. Beberapa wilayah di timur negara ini mengalami musim hujan yang berlimpah, yang menyebabkan banjir hebat dan kerusakan infrastruktur. Cuaca yang ekstrem ini mencerminkan tantangan yang dihadapi Australia dalam mengelola dan beradaptasi dengan perubahan iklim global.
+
+Periode ini juga memicu perdebatan yang berkembang tentang perlunya tindakan perlindungan lingkungan dan mitigasi perubahan iklim untuk menghadapi masalah cuaca ekstrem yang semakin sering terjadi di Australia.
                     """
                     )
 
@@ -31,5 +37,29 @@ if menuapp == "Menu Utama":
 
 if menuapp == "makan":
     st.write("Kuy")
-#tab
+
+if menuapp == "App":
+    col8,col9 = st.columns(2)
+    with col8:
+        precipitation = st.number_input("Masukan Kadar Pengendapan : ",value=0.0, step=0.1)
+        temp_max = st.number_input("Masukan Temperatur maksimal : ", value=0.0, step=0.1)
+        temp_min = st.number_input("Masukan Temperatur minimal : ",value=0.0, step=0.1)
+        wind     = st.number_input("Masukan tekanan Angin : ", value=0.0, step=0.1)
+        prediksi_cuaca = ""
+        if st.button("Prediksi Cuaca"):
+            prediksi_cuaca = model.predict([[precipitation,temp_max,temp_min,wind]])
+    with col9:
+        if prediksi_cuaca==1:
+            st.image("Assets/drizzel.png")
+            st.write("Drizzle")
+        elif prediksi_cuaca==2:
+            st.image("Assets/snow.png")
+            st.write("Snow")
+        elif prediksi_cuaca==3:
+            st.write("Rain")
+        elif prediksi_cuaca==4:
+            st.write("Sun")
+        elif prediksi_cuaca==5:
+            st.write("fog")
+        
 
